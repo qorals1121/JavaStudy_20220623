7bf66fc8577e4cdca37fe4c283efe6a26985b9ce
qorals1121 <qorals1121@naver.com>
2022-07-15 13:04:29 +0900

    j01 출력

---
+++ src/j01_출력/HelloJava.java
@@ -0,0 +1,21 @@
+package j01_출력;
+
+public class HelloJava {
+	// class명과 public class의 명이 같아야한다
+
+	public static void main(String[] args) {
+	    System.out.println("hello, Java");
+	    System.out.println("백민경");
+	    // java는 python과 다르게 들여쓰기가 없어도 실행이 가능하지만, 들여쓰기를 하지 않으면 구분하기가 힘들기 때문에 좋지않은 코드이다.
+	    // 중괄호는 무조건 열리면 닫아야한다. 코드의 끝에는 ;(세미콜론)을 써야한다.
+	    // 세미콜론으로 명령어를 끊어주기 때문에 한줄로 쭉 써도 상관은 없다.
+	    
+	    
+	    System.out.print("이름: ");
+	    System.out.println("김준일");
+	    System.out.print("연락처: ");
+	    System.out.println("010-9988-1916");
+
+	}
+
+}
---
+++ src/j01_출력/StudentInfo.java
@@ -0,0 +1,43 @@
+package j01_출력;
+
+public class StudentInfo {
+
+	public static void main(String[] args) {
+		/*
+		 * 이름 : 김준일
+		 * 학년 : 1
+		 * 
+		 * 이름 : 김준이
+		 * 학년 : 1
+		 * 
+		 * 이름 : 김준삼
+		 * 학년 : 1
+		 */
+		
+		int year;
+		year = 2; // 변수는 자료형 + 변수명이 필요하다
+		
+		int level = 10;
+		
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("학년: ");
+		System.out.println(year);
+		System.out.println();
+		
+		System.out.print("이름: ");
+		System.out.println("김준이");
+		System.out.print("학년: ");
+		System.out.println(year);
+		System.out.println();
+		
+		System.out.print("이름: ");
+		System.out.println("김준삼");
+		System.out.print("학년: ");
+		System.out.println(year);
+		//숫자는 큰따옴표를 쓰지 않아도 출력이 가능하다.
+
+
+	}
+
+}
