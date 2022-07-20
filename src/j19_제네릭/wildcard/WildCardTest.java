25940f0916f21f6c3839921520b8f072c814182a
qorals1121 <qorals1121@naver.com>
2022-07-21 01:44:46 +0900

    j19 제네릭

--- src/j19_제네릭/wildcard/WildCardTest.java
+++ src/j19_제네릭/wildcard/WildCardTest.java
@@ -1,14 +1,10 @@
 package j19_제네릭.wildcard;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
-@Builder
 public class WildCardTest<T> {
 	private T data;
-	
-
 }
--- src/j19_제네릭/wildcard/WildCardTestMain.java
+++ src/j19_제네릭/wildcard/WildCardTestMain.java
@@ -1,43 +1,39 @@
 package j19_제네릭.wildcard;
 
-import j14_추상.Human;
 import j14_추상.Animal;
+import j14_추상.Human;
 import j18_lombok.Student;
 
 public class WildCardTestMain {
 
 	public WildCardTest<?> test(int index){
 		if(index == 0) {
-			return new WildCardTest<Integer>(100);
+			return new WildCardTest<>(100);
 		}else if(index == 1) {
-			return new WildCardTest<Boolean>(true);
-		}else if (index == 2 ) {
-			return new WildCardTest<String>("문자열");
-		}else if (index == 3) {
+			return new WildCardTest<>(true);
+		}else if(index == 2) {
+			return new WildCardTest<>("문자열");
+		}else if(index == 3) {
 			return new WildCardTest<>(Student.builder().student_code(10).build());
 		}else {
-		return new WildCardTest<>(null); //비워두게 되면 object 파일로 잡히게 된다.
+			return new WildCardTest<>(null);			
 		}
 	}
 	
-
 	public WildCardTest<? extends Animal> test2() {
-		return new WildCardTest<>(new Human()); 
+		return new WildCardTest<>(new Human());
 	}
 	
-	
 	public static void main(String[] args) {
-		WildCardTestMain testmain = new WildCardTestMain();
+		WildCardTestMain testMain = new WildCardTestMain();
 		
-		System.out.println(testmain.test(0));
-		Student s = (Student) testmain.test(3).getData();
+		System.out.println(testMain.test(0));
+		Student s = (Student) testMain.test(3).getData();
 		System.out.println(s.getStudent_code());
 		
-		((Human) testmain.test2().getData()).readBooks(); // move가 Animal로 잡혀있는 이유는 애니멀을 상속 받아서 휴먼으로 다운캐스팅 해주어야한다
-		
-		
-		// http://www.tcpschool.com/java/java_collectionFramework_concept 도움이 되는 사이트
-
+		((Human) testMain.test2().getData()).readBooks();
 	}
 
 }
+
+
